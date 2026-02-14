376d9a540aed63e4e64c666c77b1f249d803ca4e
Fhmonly <rynare.of.legend@gmail.com>
2026-02-14 20:17:40 +0700

    perbaikakn type safe g

--- src/types/module/express.ts
+++ src/types/module/express.ts
@@ -1,24 +1,20 @@
-import { NextFunction, Request, Response } from 'express';
+import * as core from 'express-serve-static-core';
 import { AccessToken } from "../core/authToken";
 
-export interface CRequest extends Request {
-    user?: AccessToken;
-    safeData: unknown;
+declare global {
+    namespace Express {
+        interface Request {
+            user?: AccessToken;
+            safeData?: unknown;
+        }
+
+        interface Response {
+            customMessage?: string;
+            data?: unknown;
+        }
+    }
 }
 
-export interface CResponse extends Response {
-    customMessage?: string;
-    data?: unknown;
-}
-
-export interface CNextFunction extends NextFunction {
-
-}
-
-export interface CRequestHandler {
-    (req: CRequest, res: CResponse, next: CNextFunction): unknown;
-}
-
-const a: CRequestHandler = (req, res, next) => {
-    req.user //kenapa user tidak ada
-}   
+type CustomResponse = core.Response & {
+    json(data: core.Send): void;
+};
